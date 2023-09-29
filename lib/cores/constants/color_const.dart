f06d5cbfd8b687fbaf6da268da30eb9dbe9dc388
Putri Erika R <putrierikar902@gmail.com>
2023-09-29 22:46:39 +0700

    upload

---
+++ lib/cores/constants/color_const.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ColorConst {
+  /// * ------------------------------------------------------------------------------------ * ///
+  /// * ------------------------------------ Primary Color ------------------------- * ///
+  /// * ------------------------------------------------------------------------------------ * ///
+  static const primary50 = Color(0xffE1f5fe);
+  static const primary100 = Color(0xffB3e5fc);
+  static const primary200 = Color(0xff81d4fa);
+  static const primary300 = Color(0xff4fc3f7);
+  static const primary400 = Color(0xff29b6f6);
+  static const primary500 = Color(0xff03a9f4);
+  static const primary600 = Color(0xff0288d1);
+  static const primary700 = Color(0xff0277bd);
+  static const primary800 = Color(0xff01579b);
+  static const primary900 = Color(0xff80d8ff);
+
+  /// * ------------------------------------------------------------------------------------ * ///
+  /// * ------------------------------------ Secondary Color ------------------------------------ * ///
+  /// * ------------------------------------------------------------------------------------ * ///
+  static const secondary50 = Color(0xFFFFFCE6);
+  static const secondary100 = Color(0xFFFFF5B3);
+  static const secondary200 = Color(0xFFFFEE80);
+  static const secondary300 = Color(0xFFFFEB66);
+  static const secondary400 = Color(0xFFFFE433);
+  static const secondary500 = Color(0xFFFFDD00);
+  static const secondary600 = Color(0xFFE6C700);
+  static const secondary700 = Color(0xFFB39B00);
+  static const secondary800 = Color(0xFF806F00);
+  static const secondary900 = Color(0xFF332C00);
+  static const secondary1000 = Color(0xFFFEFEFE);
+
+  /// * ------------------------------------------------------------------------------------ * ///
+  /// * ------------------------------------ Color Text 2.0 ------------------------------------ * ///
+  /// * ------------------------------------------------------------------------------------ * ///
+
+  static const text2_50 = Color(0xffFFFFFF);
+  static const text2_100 = Color(0xffFDFDFD);
+  static const text2_200 = Color(0xffF6F6F6);
+  static const text2_300 = Color(0xffEAEAEB);
+  static const text2_400 = Color(0xffDADADA);
+  static const text2_500 = Color(0xffC3C3C4);
+  static const text2_600 = Color(0xffA6A6A6);
+  static const text2_700 = Color(0xff7F7F80);
+  static const text2_800 = Color(0xff4D4D4D);
+  static const text2_900 = Color(0xff1C1C1D);
+  static const text2_1000 = Color(0xFF44381E);
+}
